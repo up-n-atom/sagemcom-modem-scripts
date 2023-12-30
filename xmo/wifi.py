5a385d5edb508dbe563f45a6b45ca81e6c5f1593
up-n-atom <adam.jaremko@gmail.com>
2023-12-29 19:48:48 -0500

    Clean logic

--- xmo/wifi.py
+++ xmo/wifi.py
@@ -8,13 +8,14 @@ from . import xmo
 @click.pass_obj
 async def disable_wifi_radios(client: SagemcomClient, radios: tuple[str] | list[str]) -> None:
     try:
-        value = await client.get_value_by_xpath('Device/WiFi/Radios')
-        active_radios = {radio['alias'] for radio in value if radio.keys() >= {'alias', 'enable'} and \
+        _radios = await client.get_value_by_xpath('Device/WiFi/Radios')
+        active_radios = {radio['alias'] for radio in _radios \
+            if radio.keys() >= {'alias', 'enable'} and \
             radio['enable']}
-        if not len(active_radios):
+        if active_radios:
             click.echo('No active radios')
             return
-        if not len(radios):
+        if radios:
             radios = click.prompt('Choose radio', type=click.Choice(active_radios), show_choices=True),
         invalid_radios = set(radios) - active_radios
         if len(invalid_radios):
