e79d6d70d13b2b03d1212583e439c0c6a87f7a65
up-n-atom <adam.jaremko@gmail.com>
2025-09-10 21:48:38 -0400

    Add enable-wifi-radios command

--- xmo/wifi.py
+++ xmo/wifi.py
@@ -4,18 +4,14 @@ from sagemcom_api.client import SagemcomClient
 from . import xmo
 
 
-@xmo.cli.command()
-@click.option('-r', '--radios', multiple=True)
-@click.pass_obj
-async def disable_wifi_radios(client: SagemcomClient, radios: tuple[str] | list[str]) -> None:
+async def _toggle_wifi_radios(client: SagemcomClient, radios: tuple[str] | list[str], status: bool) -> None:
     try:
         _radios = await client.get_value_by_xpath('Device/WiFi/Radios')
-        # get active radios
         _radios = {radio['alias'] for radio in _radios \
             if radio.keys() >= {'alias', 'enable'} and \
-            radio['enable']}
+            radio['enable'] == status}
         if not _radios:
-            click.echo('No active radios')
+            click.echo('No active radios' if status else 'No inactive radios')
             return
         if not radios:
             radios = click.prompt('Choose radio', type=click.Choice(list(_radios) + ['all']), show_choices=True),
@@ -28,3 +24,17 @@ async def disable_wifi_radios(client: SagemcomClient, radios: tuple[str] | list[
     except Exception as e:
         client.echo(e, err=True)
         raise click.Abort()
+
+
+@xmo.cli.command()
+@click.option('-r', '--radios', multiple=True)
+@click.pass_obj
+async def disable_wifi_radios(client: SagemcomClient, radios: tuple[str] | list[str]) -> None:
+    await _toggle_wifi_radios(client, radios, True)
+
+
+@xmo.cli.command()
+@click.option('-r', '--radios', multiple=True)
+@click.pass_obj
+async def enable_wifi_radios(client: SagemcomClient, radios: tuple[str] | list[str]) -> None:
+    await _toggle_wifi_radios(client, radios, False)
