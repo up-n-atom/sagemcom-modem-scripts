2a230fb477362314ce57a03c082ec4184e1a8e12
up-n-atom <adam.jaremko@gmail.com>
2025-09-16 01:00:31 -0400

    Add read log action verification
    Checks "Device/DeviceInfo/VendorLogFiles/VendorLogFile[@uid='1']" paths' supported interfaces to see verify if the command getVendorLogDownloadURI exists

--- xmo/log.py
+++ xmo/log.py
@@ -1,6 +1,8 @@
 import time
+from typing import Any
 
 import asyncclick as click
+import dpath
 from sagemcom_api.client import SagemcomClient
 
 from . import xmo
@@ -19,24 +21,29 @@ async def flush_log(ctx: click.Context) -> None:
 @xmo.cli.command()
 @click.pass_obj
 async def read_log(client: SagemcomClient) -> None:
+    async def api_request(action: dict, param: str) -> Any | None:
+        response = await client._SagemcomClient__api_request_async([{'id': 0} | action], False)
+        params = dpath.values(response, f"reply/actions/*/callbacks/*/parameters/{param}")
+        return params[0] if params and len(params) == 1 else params
     try:
-        # uncertain if this api is universally supported by the xmo protocol... contain and
-        # improperly access the private methods of the sagemcom_api client here for now
-        actions = {
-            'id': 0,
-            'method': 'getVendorLogDownloadURI',
-            'xpath': 'Device/DeviceInfo/VendorLogFiles/VendorLogFile',
-            'parameters': {
-                'FileName': 'utilsLogFile',
-            },
-            'event-id': '1',
-        }
-        response = await client._SagemcomClient__api_request_async([actions], False)
-        data = client._SagemcomClient__get_response(response)
-        url = f"{client.protocol}://{client.host}{data['uri']}?_={time.time_ns()}"
-        async with client.session.get(url) as response:
+        method = 'getVendorLogDownloadURI'
+        xpath = "Device/DeviceInfo/VendorLogFiles/VendorLogFile[@uid='1']"
+        commands = await api_request({
+            'method': 'getValue',
+            'xpath': xpath,
+            'options': {'capability-flags': {'interface': True}}
+        }, 'capability/interfaces/*/commands/*/name')
+        if method not in (commands if isinstance(commands, list) else [commands]):
+            raise AttributeError('Command not supported by XMO client')
+        uri = await api_request({'method': method, 'xpath': xpath}, 'uri')
+        if not uri:
+            raise ValueError('Failed to retrieve log URI')
+        async with client.session.get(
+            f"{client.protocol}://{client.host}{uri}?_={time.time_ns()}"
+        ) as response:
+            response.raise_for_status()
             async for line in response.content:
-                click.echo(line, nl=False)
+                click.echo(line.decode(), nl=False)
     except Exception as e:
         click.echo(e, err=True)
         raise click.Abort()
